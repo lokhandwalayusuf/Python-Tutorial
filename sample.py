1e2e59b93aaa56b7aa9b8d44cd8fd525577f57b3
lokhandwalayusuf <54717211+lokhandwalayusuf@users.noreply.github.com>
2020-01-01 19:47:19 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -11,3 +11,5 @@ print ("THIS IS JENKINS Integration")
 print ("THIS IS JENKINS Integration - Yusuf's Project")
 
 print ("Python is now installed")
+
+print ("Running a Job on Slave 1 and Slave 2")
